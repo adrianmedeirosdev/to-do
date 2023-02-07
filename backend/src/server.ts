87ef14f2340a73569f09bc3c923f1704175ff763
Adrian Medeiros <lima.adrian13@gmail.com>
2023-02-06 21:24:02 -0300

    backend basic configs

---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import { StatusCodes } from 'http-status-codes'
+import { Request, Response } from 'express'
+
+const PORT = 3000
+
+const corsOptions = {
+  origin: 'http://localhost:5500',
+  optionsSuccessStatus: StatusCodes.OK,
+}
+const app = express()
+
+app.use(express.json())
+app.use(cors(corsOptions))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World')
+})
+
+
+app.listen(PORT, () => {
+  console.log('Is running on port ' + PORT)
+})
